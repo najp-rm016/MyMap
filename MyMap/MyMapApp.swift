c6ba5c9a225235044e9513a6ecb7024de8c19a5c
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-02-07 18:34:47 -0800

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by NSPC201admin on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
