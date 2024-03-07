c777a30bad11b01b6a4525f3164fcf5328c9972d
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-03-07 15:52:23 +0900

    福嶋さんにご確認頂くためコミット

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State var inputText: String = ""
+    @State var dispSearchKey: String = ""
+    @State var dispMapType: MKMapType = .standard
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                .onSubmit{
+                    dispSearchKey = inputText
+                }
+                .padding()
+            
+            ZStack(alignment: .bottomTrailing){
+                MapView(searchKey: dispSearchKey, mapType: dispMapType)
+                
+                Button(action: {
+                    if dispMapType == .standard{
+                        dispMapType = .satellite
+                    } else if dispMapType == .satellite{
+                        dispMapType = .hybrid
+                    } else if dispMapType == .hybrid{
+                        dispMapType = .satelliteFlyover
+                    } else if dispMapType == .satelliteFlyover{
+                        dispMapType = .hybridFlyover
+                    } else if dispMapType == .hybridFlyover{
+                        dispMapType = .mutedStandard
+                    } else{
+                        dispMapType = .standard
+                    }
+                }) {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0, alignment: .leading)
+                }
+                .padding(.trailing, 20.0)
+                .padding(.bottom, 30.0)
+            }
         }
-        .padding()
     }
 }
 
